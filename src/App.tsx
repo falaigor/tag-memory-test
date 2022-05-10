24d99b647cdd72a65956ef04165f4ad9f3d92c38
Igor Santos <igor.santos@axur.com>
2022-05-10 01:30:33 -0300

    title styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ export function App() {
     <div className="w-screen flex items-center justify-center">
       <div className="max-w-3xl w-full flex flex-col">
         <div>
-          <h1 className="text-5xl font-extrabold">Tag memory test</h1>
-          <h2>How many HTML tags can you remember?</h2>
+          <h1 className="pt-4 pb-2 text-5xl font-extrabold font-montserrat text-zinc-900">
+            Tag memory test
+          </h1>
+          <h2 className="pb-4 text-lg">How many HTML tags can you remember?</h2>
         </div>
 
         <div className="flex flex-row w-full gap-2">
