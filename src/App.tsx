685272836ddd3277556a58dd9e51dfc8acba8991
Igor Santos <igor.santos@axur.com>
2022-05-10 23:09:35 -0300

    countdow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Button } from "./components/Button/Button";
-import { Input } from "./components/Input/Input";
+import { Button } from "./components/Button";
+import { Countdown } from "./components/Contdown";
+import { Input } from "./components/Input";
 
 export function App() {
   return (
@@ -9,12 +10,29 @@ export function App() {
           <h1 className="pt-4 pb-2 text-5xl font-extrabold font-montserrat text-zinc-900">
             Tag memory test
           </h1>
-          <h2 className="pb-4 text-lg">How many HTML tags can you remember?</h2>
+          <h2 className="pb-2 text-xl">How many HTML tags can you remember?</h2>
+        </div>
+
+        <div className="flex p-4 my-4 rounded-2xl justify-between bg-zinc-100 border-2 border-zinc-900 drop-shadow-stroke items-center">
+          <p className="text-3xl font-montserrat">Time Left</p>
+
+          <Countdown />
         </div>
 
         <div className="flex flex-row w-full gap-2">
           <Input />
           <Button />
+        </div>
+
+        <div className="flex p-4 mt-4 rounded-t-2xl justify-between bg-zinc-100 border-2 border-zinc-900 items-center">
+          <p className="text-2xl font-montserrat">Time Left</p>
+        </div>
+        <div className="flex flex-col p-4 rounded-b-2xl justify-between bg-zinc-100 border-t-0 border-b-2 border-x-2 border-zinc-900">
+          <li className="list-decimal text-lg">Time Left</li>
+          <li className="list-decimal text-lg">Time Left</li>
+          <li className="list-decimal text-lg">Time Left</li>
+          <li className="list-decimal text-lg">Time Left</li>
+          <li className="list-decimal text-lg">Time Left</li>
         </div>
       </div>
     </div>
---
+++ src/components/Contdown.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+export const Countdown = () => {
+  let countdownTimeOut;
+  const [time, setTime] = useState(1 * 60);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, "0")
+    .split("");
+  const [secondLeft, secondRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
+
+  useEffect(() => {
+    if (time > 0) {
+      countdownTimeOut = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [time]);
+
+  return (
+    <div className="flex flex-row font-montserrat text-7xl">
+      <div>
+        <span>{minuteLeft}</span>
+        <span>{minuteRight}</span>
+      </div>
+      <span>:</span>
+      <div>
+        <span>{secondLeft}</span>
+        <span>{secondRight}</span>
+      </div>
+    </div>
+  );
+};
