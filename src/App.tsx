f0c10d189e3b8af7ed37141146b10eaa05fc2236
Igor Santos <falaigors@gmail.com>
2022-05-12 00:40:04 -0300

    create modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import { Fragment, useState, useEffect, useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
 import { List } from "./components/List";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
 import { Countdown } from "./components/Countdown";
+import { Modal } from "./components/Modal";
 
 import { tags } from "./tags";
-import { CaretRight } from "phosphor-react";
 
 export function App() {
   let timer = useRef(1 * 10);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [countdown, setCountdown] = useState(timer.current);
+  const [totalTime, setTotalTime] = useState(0);
   const [value, setValue] = useState("");
   const [guesstedTags, setGuesstedTags] = useState([]);
   const [startCountdown, setStartCountdown] = useState(false);
@@ -22,8 +26,9 @@ export function App() {
     if (startCountdown === true && countdown > 0) {
       setTimeout(() => {
         setCountdown((timer.current = timer.current - 1));
+        setTotalTime(totalTime + 1);
       }, 1000);
-    }
+    } else if (countdown === 0) openModal();
   }, [countdown, startCountdown]);
 
   function existsInTags(value) {
@@ -53,37 +58,51 @@ export function App() {
     }
   }
 
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
   return (
-    <div className="w-screen flex items-center justify-center">
-      <div className="max-w-3xl w-full flex flex-col">
-        <div>
-          <h1 className="pt-4 pb-2 text-5xl font-extrabold font-montserrat text-zinc-900">
-            Tag memory test
-          </h1>
-          <h2 className="pb-2 text-xl">How many HTML tags can you remember?</h2>
+    <>
+      <div className="w-screen flex items-center justify-center">
+        <div className="max-w-3xl w-full flex flex-col">
+          <div>
+            <h1 className="pt-4 pb-2 text-5xl font-extrabold font-montserrat text-zinc-900">
+              Tag memory test
+            </h1>
+            <h2 className="pb-2 text-xl">
+              How many HTML tags can you remember?
+            </h2>
+          </div>
+
+          <div className="flex p-4 my-4 rounded-2xl justify-between bg-zinc-100 border-2 border-zinc-900 drop-shadow-stroke items-center">
+            <p className="text-3xl font-montserrat">Time Left</p>
+
+            <Countdown
+              countdown={timer.current}
+              startCountdown={startCountdown}
+            />
+          </div>
+
+          <div className="flex flex-row w-full gap-2">
+            <Input
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+
+            <Button onClick={() => handleClick()} />
+          </div>
+
+          <List guesstedTags={guesstedTags} />
         </div>
-
-        <div className="flex p-4 my-4 rounded-2xl justify-between bg-zinc-100 border-2 border-zinc-900 drop-shadow-stroke items-center">
-          <p className="text-3xl font-montserrat">Time Left</p>
-
-          <Countdown
-            countdown={timer.current}
-            startCountdown={startCountdown}
-          />
-        </div>
-
-        <div className="flex flex-row w-full gap-2">
-          <Input
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-
-          <Button onClick={() => handleClick()} />
-        </div>
-
-        <List guesstedTags={guesstedTags} />
       </div>
-    </div>
+
+      <Modal isOpen={isModalOpen} closeModal={() => closeModal()} />
+    </>
   );
 }
