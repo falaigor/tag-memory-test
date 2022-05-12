47dda7e39048f770cb88b3f74e559cdb12a03708
Igor Santos <falaigors@gmail.com>
2022-05-12 11:32:23 -0300

    initial jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  testEnvironment: "jsdom",
+  collectCoverageFrom: ["<rootDir>/src/**/*.(ts|tsx)"],
+  transform: {
+    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+  },
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/src/tests/empty-module.js",
+    "\\.png$": "<rootDir>/src/tests/empty-module.js",
+  },
+};
---
+++ src/components/Button.test.tsx
@@ -0,0 +1,5 @@
+describe("Button", () => {
+  it("should render Buttton component", () => {
+    expect(true).toBe(true);
+  });
+});
