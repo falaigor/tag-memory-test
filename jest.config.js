dc13d1deb44e0c0dcfd68ac85d5b447275d67ade
Igor Santos <igor.santos@axur.com>
2022-05-15 22:06:26 -0300

    Ignore test coverage in main.tsx and tags.ts

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/tags.ts",
+    "<rootDir>/src/main.tsx",
+  ],
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   testEnvironment: "jsdom",
   collectCoverageFrom: ["<rootDir>/src/**/*.(ts|tsx)"],
