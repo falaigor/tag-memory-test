827d06504a68e76c15a83a2e8c60d6f1a6ed675b
Igor Santos <igor.santos@axur.com>
2022-05-15 22:04:48 -0300

    tests in Countdown component

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,25 +1,29 @@
-import { useState, useEffect } from "react";
-
 interface CountdownProps {
   countdown: number;
-  startCountdown: boolean;
 }
 
-export const Countdown = ({ countdown, startCountdown }: CountdownProps) => {
+export const Countdown = ({ countdown }: CountdownProps) => {
   const minutes = Math.floor(countdown / 60);
   const seconds = countdown % 60;
 
-  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
-  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+  const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, "0")
+    .split("");
+  const [secondLeft, secondRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
 
   return (
-    <div className="flex flex-row font-montserrat text-7xl">
-      <div>
+    <div
+      data-testid="countdown"
+      className="flex flex-row font-montserrat text-7xl"
+    >
+      <div data-testid="minute">
         <span>{minuteLeft}</span>
         <span>{minuteRight}</span>
       </div>
       <span>:</span>
-      <div>
+      <div data-testid="second">
         <span>{secondLeft}</span>
         <span>{secondRight}</span>
       </div>
