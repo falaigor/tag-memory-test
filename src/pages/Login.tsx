7f9284c60b6fa1c0a2ff9d3da054f9c3e83602da
Igor Santos <falaigors@gmail.com>
2022-05-25 00:10:28 -0300

    feat: button go to bak in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,19 @@
 import { useAuth } from "../contexts/auth";
 import { GithubLogo, GoogleLogo } from "phosphor-react";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Login() {
+  const navigate = useNavigate();
   const { signInGithubUrl } = useAuth();
+
+  console.log(navigate);
 
   return (
     <div
       data-testid="login-page"
       className="w-screen h-screen flex items-center justify-center mobile:p-4 "
     >
-      <div className="max-w-xs w-full flex ">
+      <div className="max-w-xs w-full flex flex-col items-center">
         <div className="flex flex-col p-4 my-4 rounded-2xl justify-center bg-zinc-100 border-2 border-zinc-900 drop-shadow-stroke items-center">
           <h1 className="text-4xl font-montserrat">Login</h1>
           <p className="text-center my-4">
@@ -41,6 +45,10 @@ export function Login() {
             Google
           </a>
         </div>
+
+        <Link to="" onClick={() => history.back()} className="underline">
+          Go to back
+        </Link>
       </div>
     </div>
   );
