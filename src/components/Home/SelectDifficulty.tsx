4381b21dcac5c5639fe3fd49e10453784ffd96f5
Igor Santos <falaigors@gmail.com>
2022-05-31 15:18:21 +0000

    select difficulty in challenge context

--- src/components/Home/SelectDifficulty.tsx
+++ src/components/Home/SelectDifficulty.tsx
@@ -1,4 +1,12 @@
+import { useChallenge } from "../../contexts/challenge";
+
 export function SelectDifficulty() {
+  const { difficultyType, setDifficultyType } = useChallenge();
+
+  function handleClick(key: string) {
+    setDifficultyType(key);
+  }
+
   return (
     <div className="py-5">
       <h4 className="text-2xl font-montserrat">Select a difficulty</h4>
@@ -11,6 +19,7 @@ export function SelectDifficulty() {
         <button
           data-testid="button"
           type="button"
+          onClick={() => handleClick("EASY")}
           className="w-full flex p-4 border-0 border-zinc-900 drop-shadow-stroke rounded-2xl text-lg font-bold justify-center items-center bg-blue-600 text-zinc-100 hover:bg-blue-800 transition-all"
         >
           Easy
@@ -19,6 +28,7 @@ export function SelectDifficulty() {
         <button
           data-testid="button"
           type="button"
+          onClick={() => handleClick("HARD")}
           className="w-full flex p-4 border-0 border-zinc-900 drop-shadow-stroke rounded-2xl text-lg font-bold justify-center items-center bg-yellow-500 text-zinc-900 hover:bg-yellow-600 transition-all"
         >
           Hard
@@ -27,6 +37,7 @@ export function SelectDifficulty() {
         <button
           data-testid="button"
           type="button"
+          onClick={() => handleClick("EXPERT")}
           className="w-full flex p-4 border-0 border-zinc-900 drop-shadow-stroke rounded-2xl text-lg font-bold justify-center items-center bg-red-600 text-zinc-100 hover:bg-red-800 transition-all"
         >
           Expert
