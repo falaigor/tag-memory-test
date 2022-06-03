baa413c9ca05625c62a2ce60d668d6e0107c2e2c
Igor Santos <falaigors@gmail.com>
2022-06-03 12:25:38 -0300

    test: create test in the Tooltip component

---
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "../../tests/test-utils";
+import { Tooltip } from "./";
+
+describe("Tooltip", () => {
+  const props = {
+    tooltipMessage: "Tooltip Message",
+    children: "Component",
+  };
+
+  it("should render Tooltip component", () => {
+    render(<Tooltip {...props} />);
+
+    expect(screen.getByTestId("tooltip")).toBeInTheDocument();
+  });
+
+  it("should show message props in Tooltip", () => {
+    render(<Tooltip {...props} />);
+
+    expect(screen.getByText(props.tooltipMessage)).toBeInTheDocument();
+  });
+});
--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -8,7 +8,10 @@ export const Tooltip = ({
   children: ReactNode;
 }) => {
   return (
-    <div className="relative flex flex-col items-center group">
+    <div
+      data-testid="tooltip"
+      className="relative flex flex-col items-center group"
+    >
       {children}
       <div className="absolute -top-5 flex flex-col items-center hidden mb-6 group-hover:flex">
         <span className="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded-md">
