6816d848e7345b9bffaabe599da9cbaa5cd8b0d7
Igor Santos <falaigors@gmail.com>
2022-06-06 08:29:38 -0300

    test: create test in Ranking listing Item

---
+++ src/components/Ranking/ListItem.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "../../tests/test-utils";
+import { RankingListItem } from "./ListItem";
+
+describe("RankingListItem", () => {
+  const props = {
+    user: {
+      name: "Igor Santos",
+    },
+    time: 432,
+    position: 1,
+    guessedTags: 79,
+  };
+
+  it("should render Ranking List Item component", () => {
+    render(<RankingListItem {...props} />);
+
+    expect(screen.getByTestId("ranking-item")).toBeInTheDocument();
+  });
+
+  it("should render component with information", () => {
+    render(<RankingListItem {...props} />);
+
+    expect(screen.getByText(/1/i)).toBeInTheDocument();
+    expect(screen.getByText(/Igor Santos/i)).toBeInTheDocument();
+    expect(screen.getByText(/79 tags/i)).toBeInTheDocument();
+    expect(screen.getByText(/432 s/i)).toBeInTheDocument();
+  });
+});
