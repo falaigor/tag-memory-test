11f728da9795c786aaa7b012c580630ea177fd00
Igor Santos <falaigors@gmail.com>
2022-07-19 00:13:22 -0300

    teste e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: '8hoq8o',
   e2e: {
     setupNodeEvents(on, config) {
       baseUrl: "http://localhost:3000";
--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -8,16 +8,112 @@ describe("App", () => {
     cy.get("body").contains("How many HTML tags can you remember?");
   });
 
-  describe("App > Change difficulty type", () => {
-    it("should change difficulty when click", () => {
+  describe("App > Select Difficulty", () => {
+    it("should change difficulty to EASY when click", () => {
+      cy.visit("http://localhost:3000");
+
       cy.get("button[data-testid='button-difficulty-easy']").click();
       cy.get("body").contains("01:00");
+    });
+
+    it("should change difficulty to HARD when click", () => {
+      cy.visit("http://localhost:3000");
 
       cy.get("button[data-testid='button-difficulty-hard']").click();
       cy.get("body").contains("00:30");
+    });
+
+    it("should change difficulty to EXPERT when click", () => {
+      cy.visit("http://localhost:3000");
 
       cy.get("button[data-testid='button-difficulty-expert']").click();
       cy.get("body").contains("00:15");
+    });
+  });
+
+  describe("App > Guessed tag", () => {
+    it("should add tag to list guessed tags", () => {
+      cy.visit("http://localhost:3000");
+
+      cy.get("input[data-testid='input']").type("HTML");
+      cy.get("button[data-testid='button']").click();
+      cy.get("body").contains("html");
+    });
+
+    it("NOT should add tag to list guessed tags", () => {
+      cy.visit("http://localhost:3000");
+
+      cy.get("input[data-testid='input']").type("HTM");
+      cy.get("button[data-testid='button']").click();
+    });
+
+    describe("Guessed Tag > Difficulty EASY", () => {
+      it("should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("input[data-testid='input']").type("HTML");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("html");
+        cy.get("body").contains("01:05");
+      });
+
+      it("NOT should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("input[data-testid='input']").type("HTM");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("01:00");
+      });
+    });
+
+    describe("Guessed Tag > Difficulty HARD", () => {
+      it("should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("button[data-testid='button-difficulty-hard']").click();
+        cy.get("body").contains("00:30");
+
+        cy.get("input[data-testid='input']").type("HTML");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("html");
+        cy.get("body").contains("00:33");
+      });
+
+      it("NOT should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("button[data-testid='button-difficulty-hard']").click();
+        cy.get("body").contains("00:30");
+
+        cy.get("input[data-testid='input']").type("HTM");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("00:30");
+      });
+    });
+
+    describe("Guessed Tag > Difficulty EXPERT", () => {
+      it("should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("button[data-testid='button-difficulty-expert']").click();
+        cy.get("body").contains("00:15");
+
+        cy.get("input[data-testid='input']").type("HTML");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("html");
+        cy.get("body").contains("00:16");
+      });
+
+      it("NOT should add tag to list guessed tags and start time countdown", () => {
+        cy.visit("http://localhost:3000");
+
+        cy.get("button[data-testid='button-difficulty-expert']").click();
+        cy.get("body").contains("00:15");
+
+        cy.get("input[data-testid='input']").type("HTM");
+        cy.get("button[data-testid='button']").click();
+        cy.get("body").contains("00:15");
+      });
     });
   });
 });
