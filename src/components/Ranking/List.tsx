c9050267bfe2f900971a326590112040c407095c
Igor Santos <falaigors@gmail.com>
2022-07-27 23:05:34 -0300

    update text

--- src/components/Ranking/List.tsx
+++ src/components/Ranking/List.tsx
@@ -49,7 +49,7 @@ export function RankingList() {
       ) : (
         <div data-testid="ranking-list">
           <div className="flex p-4 mt-4 rounded-t-2xl justify-between bg-zinc-100 border-2 border-zinc-900 items-center">
-            <p className="text-2xl font-montserrat">Top 5</p>
+            <p className="text-2xl font-montserrat">Ranking</p>
           </div>
 
           <div className="flex flex-col justify-between border-t-0 border-b-2 border-x-2 border-zinc-900 odd:bg-white even:bg-slate-100">
